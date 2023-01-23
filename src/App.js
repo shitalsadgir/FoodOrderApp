13013c11f32d6c648e8927ad7af0804a7251d7d0
shitalsadgir <sheetalsadgir@gmail.com>
2023-01-22 21:06:40 -0600

    Added Cart functinality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import {Fragment} from 'react'
+import {useState} from 'react'
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
+    const [cartIsShown, setCartIsShown]=useState(false);
+    const showCartHandler=()=>{
+        setCartIsShown(true)
+    }
+    const hideCartHandler=()=>{
+        setCartIsShown(false)
+    }
   return (
-      <Fragment>
-       <Cart></Cart>
-          <Header/>
+      <CartProvider>
+          {cartIsShown &&<Cart onClose={hideCartHandler}/>}
+          <Header onShowCart={showCartHandler}/>
           <main>
               <Meals/>
           </main>
-      </Fragment>
+      </CartProvider>
   );
 }
 
