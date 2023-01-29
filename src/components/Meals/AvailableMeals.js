ae4878bcec4ad229cabc193eb84754ba7b91b3ee
NileshJorwar <jorwarnilesh@gmail.com>
2023-01-28 19:40:25 -0600

    Updated Cart functions to add items in it

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -29,12 +29,14 @@ const DUMMY_MEALS = [
 ];
 const AvailableMeals=()=>{
     //converting dummyMeals javascript array object to jsx elements
-const mealsList=DUMMY_MEALS.map(meal=><MealItem
+const mealsList=DUMMY_MEALS.map((meal)=>(
+    <MealItem
     key={meal.id}
+    id={meal.id}
     name={meal.name}
     description={meal.description}
     price={meal.price}
-    />)
+    />));
     return<section className={classes.meals}>
        <Card>
         <ul>
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,34 @@
+import {useRef, useState} from "react";
 import classes from './MealItemForm.module.css'
 import Input from "../../UI/Input";
-const MealItemForm=props=>{
-    return <form className={classes.form}>
-       <Input label="Amount" input={{
-           id:'amount',
-           type:'number',
-           min:'1',
-           max:'5',
-           step:'1',
-           defaultvalue:'1'
-       }}/>
+
+const MealItemForm = props => {
+    const [amountIsValid, setAmountIsValid] = useState(true)
+    const amountInputRef = useRef();
+    const submitHandler = event => {
+        event.preventDefault();
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber>5) {
+            setAmountIsValid(false);
+            return;
+        }
+        props.onAddToCart(enteredAmountNumber);
+    };
+
+    return (<form className={classes.form} onSubmit={submitHandler}>
+        <Input
+            ref={amountInputRef}
+            label="Amount" input={{
+            id: 'amount',
+            type: 'number',
+            min: '1',
+            max: '5',
+            step: '1',
+            defaultvalue: '1'
+        }}/>
         <button>+ Add</button>
-    </form>
-}
+        {!amountIsValid && <p>Please enter valid amount</p>}
+    </form>);
+};
 export default MealItemForm;
